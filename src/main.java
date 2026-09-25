e03096011841a0282a0f3d23128455e5f7bf14ff
JajkaMinimajka67 <diabloxovy731@gmail.com>
2026-09-25 11:24:48 +0200

    powtorzenie

--- src/main.java
+++ src/main.java
@@ -1,2 +1,16 @@
 public class main {
+    public static void main(String[] args) {
+
+    }
 }
+// 1. wylosuj 100 liczb z zakresu od 1 do 50
+// 2. zapisz liczby w liscie
+// 3. policz ile jets wartosci unikatowych
+// 4. zapisz liste tylko z wartosciami parzystymi
+// 5. obliczile jest liczb pierwszych wylosowanych
+
+
+// 1. zrob klase ksiazka z polem tytul ,autor, czydostepna
+// 2. zrob klase czytelnik z polem imie , wypozyczone ksiazki
+// 3. zrob klase biblioteka z polami ksiazki, czytelnicy
+// 4. zaprogramuj wszystko co potrzebujesz aby zrobic metode wypozycz, po wypozyczeniu ma byc niedostepna, ma byc u czytelnika w liscie ksiazek
